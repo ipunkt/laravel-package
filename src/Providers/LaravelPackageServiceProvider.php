2d9c0b1ed97b7165243413b93855cf01b7581eee
rokde <github@mail.robert-kummer.de>
2017-10-29 14:33:38 +0100

    adding service provider

---
+++ src/Providers/LaravelPackageServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ipunkt\LaravelPackage\Providers;
+
+use Illuminate\Support\AggregateServiceProvider;
+
+class LaravelPackageServiceProvider extends AggregateServiceProvider
+{
+    /**
+     * The provider class names.
+     *
+     * @var array
+     */
+    protected $providers = [];
+}
