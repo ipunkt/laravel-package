b7f344aaa4a8e3a7cd354e4e8b005cc0e49030eb
Robert Kummer <github@mail.robert-kummer.de>
2017-11-01 12:34:05 +0100

    supporting package path feature

--- src/Providers/ConfigurationProvider.php
+++ src/Providers/ConfigurationProvider.php
@@ -6,6 +6,8 @@ use Ipunkt\Laravel\PackageManager\Providers\ConfigurationServiceProvider;
 
 class ConfigurationProvider extends ConfigurationServiceProvider
 {
+    protected $packagePath = __DIR__ . '/../../';
+
     protected $configurationFiles = [
         LaravelPackageServiceProvider::NAMESPACE => 'config/config.php',
     ];
