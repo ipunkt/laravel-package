e8841069a98f5ad7506b1367254cdfdb0a03e2b2
Robert Kummer <github@mail.robert-kummer.de>
2017-11-01 12:35:40 +0100

    Supporting package path feature

--- src/Providers/ViewProvider.php
+++ src/Providers/ViewProvider.php
@@ -6,6 +6,8 @@ use Ipunkt\Laravel\PackageManager\Providers\ViewServiceProvider;
 
 class ViewProvider extends ViewServiceProvider
 {
+    protected $packagePath = __DIR__ . '/../../';
+
     protected $namespace = LaravelPackageServiceProvider::NAMESPACE;
 
     protected $templatesFolder = 'resources/views';
