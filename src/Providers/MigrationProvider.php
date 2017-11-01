bc61396adb2f2c9156a29a6ce3e8dcc97ef5d941
Robert Kummer <github@mail.robert-kummer.de>
2017-11-01 12:34:37 +0100

    Supporting package path feature

--- src/Providers/MigrationProvider.php
+++ src/Providers/MigrationProvider.php
@@ -6,5 +6,7 @@ use Ipunkt\Laravel\PackageManager\Providers\MigrationServiceProvider;
 
 class MigrationProvider extends MigrationServiceProvider
 {
+    protected $packagePath = __DIR__ . '/../../';
+    
     protected $migrationsFolder = 'database/migrations';
 }
