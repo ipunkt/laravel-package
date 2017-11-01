df8664339798a3cbeb86819a4531c053fcbe4c51
Robert Kummer <github@mail.robert-kummer.de>
2017-11-01 12:35:17 +0100

    Supporting package path feature

--- src/Providers/TranslationProvider.php
+++ src/Providers/TranslationProvider.php
@@ -6,6 +6,8 @@ use Ipunkt\Laravel\PackageManager\Providers\TranslationServiceProvider;
 
 class TranslationProvider extends TranslationServiceProvider
 {
+    protected $packagePath = __DIR__ . '/../../';
+
     protected $namespace = LaravelPackageServiceProvider::NAMESPACE;
 
     protected $translationsFolder = 'resources/lang';
